059495bd69a2bf9771a0b26da38713b94f5a0450
serife <eyaman895@gmail.com>
2024-04-22 00:12:52 +0300

    fix: responsive design

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -4,11 +4,11 @@ import MapImage from "assets/images/Map.png";
 const Map = () => {
   return (
     <div className="bg-amber-900 ">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-12 py-12 relative">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-12 lg:py-12 py-28 relative">
         <img src={MapImage} alt="map" className="w-[980px]" />
-        <span className="text-white space-y-4 absolute top-[45%] right-[25%]">
-          <p className="text-8xl font-extrabold">11,658,467</p>
-          <p className="text-6xl font-extrabold">Shoes Collected</p>
+        <span className="text-white space-y-4 absolute lg:top-[45%] lg:right-[25%] top-[55%] right-[10%] lg:text-start text-center">
+          <p className="lg:text-8xl text-5xl font-extrabold">11,658,467</p>
+          <p className="lg:text-6xl text-4xl font-extrabold">Shoes Collected</p>
         </span>
       </div>
     </div>
